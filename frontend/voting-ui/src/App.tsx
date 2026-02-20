4433c15ab31d8e64d9c9a2d20bb9e0663d663a74
ibs13 <ibm.sabbir@gmail.com>
2026-02-21 00:45:44 +0100

    feat: protect ballot and success routes

--- frontend/voting-ui/src/App.tsx
+++ frontend/voting-ui/src/App.tsx
@@ -3,14 +3,30 @@ import { EmailPage } from "./pages/EmailPage";
 import { OtpPage } from "./pages/OtpPage";
 import { BallotPage } from "./pages/BallotPage";
 import { SuccessPage } from "./pages/SuccessPage";
+import { ProtectedRoute } from "./components/ProtectedRoute";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<EmailPage />} />
       <Route path="/otp" element={<OtpPage />} />
-      <Route path="/ballot" element={<BallotPage />} />
-      <Route path="/success" element={<SuccessPage />} />
+      <Route
+        path="/ballot"
+        element={
+          <ProtectedRoute>
+            <BallotPage />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/success"
+        element={
+          <ProtectedRoute>
+            <SuccessPage />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 };
