2ed5e11b4f357fd6582d3060f2373cf01c82f714
ibs13 <ibm.sabbir@gmail.com>
2026-02-21 02:06:17 +0100

    refactor: use protected layout for shared logout and header

--- backend/Voting.Api/obj/Debug/net10.0/Voting.Api.AssemblyInfo.cs
+++ backend/Voting.Api/obj/Debug/net10.0/Voting.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Voting.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dee1ba64aa4bb29df89203cef88e2b3d6f1b7470")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67e66c3f5a17616a628d524d66e7ef7fd114e723")]
 [assembly: System.Reflection.AssemblyProductAttribute("Voting.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Voting.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/voting-ui/src/App.tsx
+++ frontend/voting-ui/src/App.tsx
@@ -5,38 +5,48 @@ import { BallotPage } from "./pages/BallotPage";
 import { SuccessPage } from "./pages/SuccessPage";
 import { ProtectedRoute } from "./components/ProtectedRoute";
 import { ResultsPage } from "./pages/ResultsPage";
+import ProtectedLayout from "./layouts/ProtectedLayout";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<EmailPage />} />
       <Route path="/otp" element={<OtpPage />} />
-      <Route
-        path="/ballot"
-        element={
-          <ProtectedRoute>
-            <BallotPage />
-          </ProtectedRoute>
-        }
-      />
 
       <Route
-        path="/success"
         element={
           <ProtectedRoute>
-            <SuccessPage />
+            <ProtectedLayout />
           </ProtectedRoute>
         }
-      />
+      >
+        <Route
+          path="/ballot"
+          element={
+            <ProtectedRoute>
+              <BallotPage />
+            </ProtectedRoute>
+          }
+        />
 
-      <Route
-        path="/results"
-        element={
-          <ProtectedRoute>
-            <ResultsPage />
-          </ProtectedRoute>
-        }
-      />
+        <Route
+          path="/success"
+          element={
+            <ProtectedRoute>
+              <SuccessPage />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/results"
+          element={
+            <ProtectedRoute>
+              <ResultsPage />
+            </ProtectedRoute>
+          }
+        />
+      </Route>
     </Routes>
   );
 };
